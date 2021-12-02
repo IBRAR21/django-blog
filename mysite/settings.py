eb441ab2d726f4ef37827a372111c7f5982e9b73
IBRAR21 <inderjotbrar92@gmail.com>
2021-12-01 17:26:46 -0800

    Applied black.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,13 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.sites',  # new
-
-    'allauth',  # new
-    'allauth.account',  # new
-    'allauth.socialaccount',  # new
-    'allauth.socialaccount.providers.github',
-
+    "django.contrib.sites",  # new
+    "allauth",  # new
+    "allauth.account",  # new
+    "allauth.socialaccount",  # new
+    "allauth.socialaccount.providers.github",
     "polling",
     "blogging",
 ]
@@ -146,6 +144,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = "none"
 
 LOGIN_REDIRECT_URL = "/"
